2aa48b4b619cbf4df598a9d64eabca8c5dab086a
berkayyurttas <byurttas2004@gmail.com>
2025-12-09 18:19:44 +0300

    ROS2 ve Gazebo dosyaları eklendi

---
+++ build/simple_py_pkg/share/simple_py_pkg/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/berkay/ros2_ws/build/simple_py_pkg"
---
+++ src/simple_py_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'simple_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='berkay',
+    maintainer_email='berkay@todo.todo',
+    description='Basit bir ROS 2 Python publisher örneği',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = simple_py_pkg.talker:main',
+        ],
+    },
+)
+
